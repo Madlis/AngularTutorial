ad3493d65b2d02de1b41c15bff9ec354bd84d27f
Boghdan Pavlov <lukures@yandex.ru>
2016-05-15 15:27:08 +0300

    step - 02

--- project/build/app/app.js
+++ project/build/app/app.js
@@ -1,10 +1,12 @@
-$.material.init();
+// $.material.init();
 //controller(name,constructor)
 // name - {string} - Имя коетроллера. constructor - {Function}.
-angular.module('app', [])
-        .controller('FirstCtrl', function FirstCtrl($log) {
-            var first = this;
+angular.module("app",[])
+        .controller('FirstCtrl', FirstCtrl)
+        .controller('SecondCtrl',SecondCtrl);
+        function FirstCtrl($scope) {
+        
+        }
+        function SecondCtrl($scope){
 
-            first.greeting = "First";
-            $log.log(first);
-        })
+        }
