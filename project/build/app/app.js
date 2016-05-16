8adcb75f99c3bdc09c447b4d97748b9fa70523fc
Boghdan Pavlov <lukures@yandex.ru>
2016-05-16 20:43:15 +0300

    step 06 Filtres

--- project/build/app/app.js
+++ project/build/app/app.js
@@ -2,12 +2,27 @@
 //controller(name,constructor)
 // name - {string} - Имя коетроллера. constructor - {Function}.
 //config(configFn) - Используйте этот метод для регистрации работы, которую нужно сделать во время загрузки модулю.
+
+// filter(name, filterFactory)
+
+
+// Параметры
+// name – {string} –
+// Имя фильтра.
+
+// filterFactory – {Function} –
+// Фабричная функция для создания нового экземпляра фильтра.
 var myApp = angular.module('myApp',[]);
 
 myApp.controller('FirstCtrl',FirstCtrl)
     .controller('SecondCtrl',SecondCtrl)
     .service('Data', function () {
         return {message: "I'm data from a service"};
+    })
+    .filter('reverse', function(Data){
+        return function(text){
+            return text.split("").reverse().join("") + Data.message;
+        }
     });
 
 function FirstCtrl($scope, Data){
