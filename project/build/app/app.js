2011fbef3f16c893f0133ef8e7d0c76c9f16849a
Boghdan Pavlov <lukures@yandex.ru>
2016-05-16 22:33:39 +0300

    step 10 Directive

--- project/build/app/app.js
+++ project/build/app/app.js
@@ -86,3 +86,12 @@ function AvengersCtrl(Avengers) {
   var avengersCtrl = this;
     avengersCtrl.avengers = Avengers;
 }
+
+var app = angular.module("superhero", [])
+
+app.directive('superman', function () {
+    return {
+        restrict: 'E',
+        template: "<div> Here I am to save the day</div>"
+    };
+})
