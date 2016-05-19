5abdfbda30c3979e012e79b801094b0004dd7de4
Boghdan Pavlov <lukures@yandex.ru>
2016-05-19 17:44:28 +0300

    step 16 Understading IsolateScope

--- project/src/app/directive.js
+++ project/src/app/directive.js
@@ -10,66 +10,26 @@
 
 
 
-var app = angular.module("superApp", []);
+var app = angular.module("choreApp", []);
 
+app.controller("ChoreCtrl", function() {
+    var choreCtrl = this; 
+    choreCtrl.logChore = function(chore) {
+        alert(chore + " is done!");
+    };
+});
 
-app.controller("SuperHeroCtrl", function($element) {
-  var superman = this;
-  
-  superman.abilities = [];
-
-  superman.addStrength = function() {
-    superman.abilities.push("strength");
-  };
-
-  superman.addSpeed = function() {
-    superman.abilities.push("speed");
-  };
-
-  superman.addFlight = function() {
-    superman.abilities.push("flight");
-  };
-  
-  $element.addClass("btn");
-  $element.bind("mouseenter", function() {
-    console.log(superman.abilities);
-  });
-})
-
-app.directive("superhero", function() {
+app.directive("kid", function() {
     return {
         restrict: "E",
-        scope: {},
-
-        controller: "SuperHeroCtrl as superhero"
-  };
-});
-
-app.directive("strength", function() {
-    return {
-        require: "superhero",
-        link: function(scope, element, attrs, superheroCtrl) {
-            superheroCtrl.addStrength();
-        }
+        scope: {
+            done: "&"
+        },
+        template: '<input type="text" ng-model="chore">' +
+          ' {{chore}}' +
+          ' <div class="button" ng-click="done({chore:chore})">I\'m done!</div>'
     };
 });
 
-app.directive("speed", function() {
-    return {
-        require: "superhero",
-        link: function(scope, element, attrs, superheroCtrl) {
-            superheroCtrl.addSpeed();
-        }
-    };
-});
-
-app.directive("flight", function() {
-    return {
-        require: "superhero",
-        link: function(scope, element, attrs, superheroCtrl) {
-            superheroCtrl.addFlight();
-        }
-    };
-});
 
 
