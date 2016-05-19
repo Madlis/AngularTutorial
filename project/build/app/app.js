fb0bc5dcb81f14a215a3ff20bdf4835b96efed63
Boghdan Pavlov <lukures@yandex.ru>
2016-05-19 17:28:42 +0300

    step 15 directive to directive

--- project/build/app/app.js
+++ project/build/app/app.js
@@ -87,7 +87,7 @@ function AvengersCtrl(Avengers) {
     avengersCtrl.avengers = Avengers;
 }
 
-var app = angular.module("behaviorApp", [])
+
 // http://stepansuvorov.com/blog/2013/06/start-with-angularjs-part2/
 // restrict     задает способ встраивания(смотри выше):
 // E – тэг(имя элемента)
@@ -97,19 +97,68 @@ var app = angular.module("behaviorApp", [])
 // Директива – это что-то типа вспомогательного элемента для отрисовки представления. Может быть встроена в HTML следующими способами:
 // В виде атрибута тега    <div directive=”expression”>
 
-app.directive('enter', function () {
-    // directive private methods here
-    return function ($scope, element, attrs){
-        //directive logic
-        element.bind('mouseenter', function(){
-            element.addClass(attrs.enter);
-        })
-    }
+
+
+var app = angular.module("superApp", []);
+
+
+app.controller("SuperHeroCtrl", function($element) {
+  var superman = this;
+  
+  superman.abilities = [];
+
+  superman.addStrength = function() {
+    superman.abilities.push("strength");
+  };
+
+  superman.addSpeed = function() {
+    superman.abilities.push("speed");
+  };
+
+  superman.addFlight = function() {
+    superman.abilities.push("flight");
+  };
+  
+  $element.addClass("btn");
+  $element.bind("mouseenter", function() {
+    console.log(superman.abilities);
+  });
 })
-app.directive('leave', function () {
-    return function ($scope, element, attrs){
-        element.bind('mouseleave', function(){
-            element.removeClass(attrs.enter);
-        })
-    }
-})
+
+app.directive("superhero", function() {
+    return {
+        restrict: "E",
+        scope: {},
+
+        controller: "SuperHeroCtrl as superhero"
+  };
+});
+
+app.directive("strength", function() {
+    return {
+        require: "superhero",
+        link: function(scope, element, attrs, superheroCtrl) {
+            superheroCtrl.addStrength();
+        }
+    };
+});
+
+app.directive("speed", function() {
+    return {
+        require: "superhero",
+        link: function(scope, element, attrs, superheroCtrl) {
+            superheroCtrl.addSpeed();
+        }
+    };
+});
+
+app.directive("flight", function() {
+    return {
+        require: "superhero",
+        link: function(scope, element, attrs, superheroCtrl) {
+            superheroCtrl.addFlight();
+        }
+    };
+});
+
+
