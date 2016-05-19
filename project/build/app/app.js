43e7e75b4d85bf319d67111e3732b7eaec25377d
Boghdan Pavlov <lukures@yandex.ru>
2016-05-20 00:31:50 +0300

    step 21 Transclusion Basics

--- project/build/app/app.js
+++ project/build/app/app.js
@@ -109,29 +109,18 @@ function AvengersCtrl(Avengers) {
 var app = angular.module("phoneApp", []);
 
 app.controller("AppCtrl", function($scope) {
-    $scope.leaveVoicemail = function(number, message) {
-        alert("Number: " + number + " said: " + message);
-    };
+    var appctrl = this;
 });
 
-app.directive("phone", function() {
+app.directive('panel', [function () {
     return {
-        restrict: "E",
-        scope: {
-            number: "@",
-            network: "=",
-            makeCall: "&"
-        },
-        template: '<div class="panel">Number: {{number}} Network:<select ng-model="network" ng-options="net for net in networks">' +
-          '<input type="text" ng-model="value">' +
-          '<div class="button" ng-click="makeCall({number: number, message: value})">Call home!</div></div>',
-
-        link: function(scope) {
-            scope.networks = ["Verizon", "AT&T", "Sprint"];
-            scope.network = scope.networks[0];
-        }
+        restrict: 'E',
+         transclude: true, //Передача содержимого из директивы шаблону включается очень просто. Чтобы это сделать, просто установите transclude: true:
+        template: '<div class="panel" ng-transclude>This is a panel companent</div>'
     };
-});
+}])
+
+
 
 
 
