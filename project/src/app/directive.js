c80828a03963613bd0fc1ea1b4a62b81de90b4d0
Boghdan Pavlov <lukures@yandex.ru>
2016-05-19 22:47:56 +0300

    step 19

--- project/src/app/directive.js
+++ project/src/app/directive.js
@@ -16,21 +16,25 @@
 // = – двустороннее связывание значения атрибута и переменной
 // & – позволяет выполнять выражения из аттрибута в рамках родительского scope
 
-var app = angular.module("drinkApp", []);
+var app = angular.module("phoneApp", []);
 
 app.controller("AppCtrl", function() {
-    var appctrl = this; 
-    appctrl.ctrlFlavor = "blackberry";
-})
-
-app.directive("drink", function() {
-    return {
-        scope: {
-            flavor: "=" 
-        },
-        template: '<input type="text" ng-model="flavor">'
+    var appctrl = this;
+    appctrl.callHome = function(message) {
+        alert(message);
     };
 })
+
+app.directive("phone", function() {
+    return {
+        scope: {
+            dial: "&"
+        },
+        template: '<input type="text" ng-model="value">' +
+          '<div class="button" ng-click="dial({message:value})">Call home!</div>'
+    };
+})
+
 
 
 
